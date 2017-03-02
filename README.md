4ad2e1ff3fb13ce6a06fafcb428f048a77174a9d
danieljosesilva <djsilva99@gmail.com>
2017-03-02 17:03:21 +0000

    first commit with physplot.py and README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # physplot
+
+The physplot class allows one to load data from txt files, make simple treatment and plot the data in a fast and efficient way. This program is usefull when handling with output files from numerical simulations in a quick way or if one wants to make a quick plot from txt files to watch the content. This project is dynamic, i.e. is being modified to fulfill needs without defined periodicity. Below are the attributes and methods of physplot.
+
+attributes:
+
+self.header -> saves the data of the first line pointed out in in initialization
+self.numberColumns -> saves the number of columns present in the object
+self.data -> saves all the values present in the file. each entry corresponds to one line, Each entry is a list of values 
+	corresponding to each column
+
+methods:
+
+removePeriodicPoints -> allows to remove periodic points for specific columns.
+plot -> plots the columnY as a function of columnX
+
+
+Example:
--- physplot.py
+++ physplot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 class physplot:
 
-	def __init__(self,fileName,start=0,stop=-1,delim=','):
+	def __init__(self,fileName,start=0,stop=-1,delim='\t'):
 
 		f=open(file,'r')
 		lines=f.readlines()
@@ -46,9 +46,6 @@ class physplot:
 				n=n+1
 
 
-
-
-
 	def plot(self,xColumn,yColumns):
 
 		if xColumn==-1:
@@ -60,35 +57,3 @@ class physplot:
 		plt.show()		
 
 
-"""
-
-
-
-#file to be read
-file='M6_l2_C5L3_0004.lvm'
-f=open(file,'r')
-lines=f.readlines()
-
-#reads and selects the data from endurence tests
-Ron=[]
-Roff=[]
-for i in range(1,len(lines)):
-	if i%10==0:
-		Roff.append(float(lines[i].split('\t')[2]))
-	if (i+5)%10==0:
-		Ron.append(float(lines[i].split('\t')[2]))
-f.close()
-
-#saves the data to file savedFile.csv
-savedFile=open('savedFile.csv','w')
-for i in range(len(Ron)):
-	savedFile.write(str(i)+','+str(Ron[i])+','+str(Roff[i])+'\n')
-savedFile.close()
-
-#plots the data
-plt.plot(range(len(Ron)),Ron,'ro')
-plt.plot(range(len(Roff)),Roff,'bo')
-plt.show()
-
-
-"""
