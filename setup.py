81461451ab876592f36a1bf7fa5a0a555dee949f
Unknown <djsilva99@gmail.com>
2017-11-10 21:20:27 +0000

    Updating documentation

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-"""physplotlib: Library for plotting quickly data from log files
+"""physplotlib: Library for plotting data, in particular from log files
 
 Usefull when handling with output files from numerical
 simulations in a quick way. Loads data from text files, makes treatments
@@ -13,14 +13,13 @@ from setuptools import setup
 
 setup(name='physplotlib',
       version='0.1.1',
-      description='Library for plotting quickly data from log files',
+      description='Library for plotting data from log files',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 2.7',
           'Topic :: Scientific/Engineering :: Physics',
       ],
-      keywords='plotting numerical simulations heatrapy log files',
       url='https://github.com/danieljosesilva/physplotlib',
       author='Daniel Silva',
       author_email='djsilva99@gmail.com',
